c6ae8c304f75b67514c9eb1c3859ebe8b169ea70
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-09 16:54:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching (name, str){
+    const filtered = name.filter( name => name.toLowerCase() === str.toLowerCase());
+   return filtered;
+}
+function fuzzyMatch (driver, str) {
+    const filtered = driver.filter( name => name.toLowerCase().indexOf(str.toLowerCase()) === 0);
+    return filtered;
+}
+function matchName (driver, str) {
+    const filtered = driver.filter(name => name['name'].toLowerCase() === str.toLowerCase() );
+    return filtered
+}
